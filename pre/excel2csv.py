cdf6cb00d0d297dd97ffa8e87def69614d0a70ed
song liu <song.liu@ia.ac.cn>
2020-02-05 11:44:08 +0800

    see #1, excel to csv

---
+++ pre/excel2csv.py
@@ -0,0 +1,27 @@
+import xlrd
+
+
+def main():
+    wb = xlrd.open_workbook("../data/1.xlsx")
+    sheet = wb.sheet_by_index(0)
+    total_row_num = sheet.nrows
+    total_col_num = sheet.ncols
+
+    data_list = list()
+    # read data from excel
+    for i in range(1, total_row_num):
+        tem_list = list()
+        for j in range(total_col_num):
+            tem_list.append(str(sheet.cell_value(i, j)))
+        tem_list[3], tem_list[(total_col_num-1)] = tem_list[(total_col_num-1)], tem_list[3]
+        data_list.append(','.join(tem_list))
+
+    # write data to csv
+    with open("../data/1.csv", "w") as fp:
+        for i in range(len(data_list)):
+            fp.write(data_list[i])
+            fp.write("\n")
+
+
+if __name__ == '__main__':
+    main()
